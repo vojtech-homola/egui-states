48dd336ecd9f43b8ba2a83797008f7c653d9b68f
vojtech-homola <vojhom@outlook.cz>
2024-10-06 17:48:48 +0200

    feat: add version, license and readme

---
+++ REDME.md
@@ -0,0 +1,3 @@
+# egui-pysync: a server/client for synchronizing states between python and egui UIs
+
+TODO: Write a project documentation
--- egui_pysync/__init__.py
+++ egui_pysync/__init__.py
@@ -1,8 +1,8 @@
-"""Doc string."""
+"""TODO: Doc string."""
 
-from egui_pysync.typing import SteteServerCoreBase
+from egui_pysync import structures, version
 from egui_pysync.server import StateServer
-from egui_pysync import structures
+from egui_pysync.typing import SteteServerCoreBase
+from egui_pysync.version import __version__
 
-
-__all__ = ["SteteServerCoreBase", "StateServer", "structures"]
+__all__ = ["version", "__version__", "SteteServerCoreBase", "StateServer", "structures"]
