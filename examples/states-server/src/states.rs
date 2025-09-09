0d971ccd4f81d7f2f4237b8e57f453d4f8ff3420
vojtech-homola <vojhom@outlook.cz>
2025-09-09 06:31:13 +0200

    delete gitignore file

--- examples/states-server/src/states.rs
+++
@@ -1,9 +0,0 @@
-// Ganerated by build.rs, do not edit
-
-use egui_states_pyserver::ServerValuesCreator;
-
-pub(crate) fn create_states(c: &mut ServerValuesCreator) {
-    c.add_value::<f32>(0.0);
-    c.add_image();
-    c.add_graphs::<f32>();
-}
