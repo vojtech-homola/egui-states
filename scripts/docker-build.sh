e2935d589085e56ab662ac59b0c5ebf97c5a61ca
vojtech-homola <vojhom@outlook.cz>
2026-02-05 21:30:00 +0100

    fix build scripts

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -5,8 +5,8 @@ docker run --rm -v "$PWD:/io" -w /io quay.io/pypa/manylinux_2_28_x86_64 bash -lc
   curl -LsSf https://astral.sh/uv/install.sh | sh
   export PATH="$HOME/.local/bin:$PATH"
 
-  uv build --wheel -p 3.12
-  uv build --wheel -p 3.13
-  uv build --wheel -p 3.14
-  uv build --wheel -p 3.14t
+  uv build --wheel -p 3.12 -C maturin.build-args="--manylinux 2_28 --auditwheel repair --compression-level 9"
+  uv build --wheel -p 3.13 -C maturin.build-args="--manylinux 2_28 --auditwheel repair --compression-level 9"
+  uv build --wheel -p 3.14 -C maturin.build-args="--manylinux 2_28 --auditwheel repair --compression-level 9"
+  uv build --wheel -p 3.14t -C maturin.build-args="--manylinux 2_28 --auditwheel repair --compression-level 9"
 '
